58f39cb189bb2598d81b51d29485691ba6b4a202
stevenhan777 <120138752+stevenhan777@users.noreply.github.com>
2026-06-08 08:18:23 -0400

    Created stg_lichess__games.sql and stg_lichess__moves.sql within staging folder to do basic data cleaning and filtering and joining games, moves and move_evals tables together

---
+++ chess_analytics/models/staging/stg_lichess__games.sql
@@ -0,0 +1,43 @@
+-- models/staging/stg_lichess__games.sql
+with source as (
+
+    select * from {{ source('lichess_raw', 'games') }}
+
+),
+
+cleaned as (
+
+    select
+        game_id,
+        lower(white_username)      as white_username,
+        lower(black_username)      as black_username,
+        white_rating,
+        black_rating,
+        white_rating_diff,
+        black_rating_diff,
+        white_accuracy,
+        black_accuracy,
+        winner,
+        coalesce(winner, 'draw')   as result,
+        status,
+        perf_type,
+        coalesce(variant, 'standard') as variant,
+        opening_eco,
+        opening_name,
+        opening_ply,
+        time_control_initial,
+        time_control_increment,
+        concat(
+            cast(time_control_initial / 60 as string), '+',
+            cast(time_control_increment    as string)
+        )                          as time_control_label,
+        timestamp_millis(created_at)   as game_created_at,
+        timestamp_millis(last_move_at) as game_ended_at,
+        ingested_at
+
+    from source
+    where time_control_initial > 0   -- exclude untimed games
+
+)
+
+select * from cleaned
---
+++ chess_analytics/models/staging/stg_lichess__moves.sql
@@ -0,0 +1,50 @@
+-- models/staging/stg_lichess__moves.sql
+with moves as (
+
+    select
+        game_id,
+        move_offset,
+        move_number,
+        color,
+        lower(username) as username,
+        move_san
+    from {{ source('lichess_raw', 'moves') }}
+
+),
+
+evals as (
+
+    select
+        game_id,
+        move_offset,
+        eval_score,
+        mate_in,
+        best_move,
+        variation,
+        judgment,
+        time_left_cs,
+        time_left_ratio
+    from {{ source('lichess_raw', 'move_evals') }}
+
+)
+
+-- Simple JOIN on the composite key
+select
+    m.game_id,
+    m.move_offset,
+    m.move_number,
+    m.color,
+    m.username,
+    m.move_san,
+    e.eval_score,
+    e.mate_in,
+    e.best_move,
+    e.variation,
+    e.judgment,
+    e.time_left_cs,
+    e.time_left_ratio
+
+from moves m
+left join evals e
+    on  m.game_id     = e.game_id
+    and m.move_offset = e.move_offset
